1ebdb58a57e014f77108abfce58e12c82593d3fb
Andrew Roth <andrewroth@gmail.com>
2015-03-24 23:53:38 -0400

    manually build the sql because the to_sql seems to just return an empty string now

--- lib/scope_cache_key.rb
+++ lib/scope_cache_key.rb
@@ -17,7 +17,8 @@ module ScopeCacheKey
   #   Item.active.cache_key # => "0b27dac757428d88c0f3a0298eb0278e"
   #
   def cache_key
-    scope_sql = where(nil).select("#{table_name}.id, #{table_name}.updated_at").to_sql
+    #scope_sql = where(nil).select("#{table_name}.id, #{table_name}.updated_at").to_sql
+    scope_sql = "select #{table_name}.id, #{table_name}.updated_at from #{table_name}"
 
     sql = "SELECT md5(array_agg(id || '-' || updated_at)::text) " +
           "FROM (#{scope_sql}) as query"
